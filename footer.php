72ce5d8208ce8d1149892e6eb611e161c6631b82
Artur <artur.solomonik@gmail.com>
2024-03-11 13:03:47 +0100

    Added first basic layout

--- footer.php
+++ footer.php
@@ -13,17 +13,10 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'rampsow' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'rampsow' ), 'WordPress' );
-				?>
-			</a>
+			<strong>ACCESSTECH 2024</strong>
 			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'rampsow' ), 'rampsow', '<a href="https://github.com/Arduqq">Artur Solomonik</a>' );
-				?>
+			<a href="impressum">Impressum</a>
+			<a href="privacy-policy">Privacy Policy</a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
